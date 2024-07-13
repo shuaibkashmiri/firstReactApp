a6e33ec6547e5682057d4884e615824b95514b52
shuaibkashmiri <114304104+shuaibkashmiri@users.noreply.github.com>
2024-07-13 23:54:06 +0530

    Routes Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,34 @@ import React from "react";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-function App() {
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Services from "./components/Services"
+import Nopage from "./components/Nopage.jsx"
+import{BrowserRouter, Routes,Route} from "react-router-dom"
+
+
+const App = () => {
   return (
     <>
+     <BrowserRouter>
       <Navbar />
-      <Home/>
+     <Routes>
+     <Route path="*" element= {<Nopage/>} />    
+          <Route path="/" element= {<Home/>} />
+          <Route path="/contact" element= {<Contact/>} />
+          <Route path="/about" element= {<About/>} />
+          <Route path="/services" element= {<Services/>} />
+ </Routes>
+      
       <Footer />
-    </>
+      </BrowserRouter>
+      </>
+      
   );
 }
+
+
 
 export default App;
 
