38f0f516fefcdd7af3fe2d1cbac3455c611d9895
shuaibkashmiri <114304104+shuaibkashmiri@users.noreply.github.com>
2024-08-14 23:03:33 +0530

    menu-item-added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Navbar.scss";
 import {Link} from "react-router-dom"
 import logo from "../images/logo.png"
 import { IoMdMenu } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+const [menuData, setMenuData]=useState(false)
+
+
+const toggleMenu =()=>{
+  setMenuData(!menuData)
+}
+
   return (
     <div className="navbar">
 
-    <IoMdMenu className="menu" />
+    <IoMdMenu className="menu" onClick={toggleMenu}/>
+
+    <div className={menuData ? "drop-down" : "display-none"} >
+            <div className="close-btn"><IoClose onClick={toggleMenu} /></div>
+            <ul>
+              <li>
+                <Link to="/"> Home</Link>
+              </li>
+              <li>
+                <Link to="/contact"> Contact</Link>
+              </li>
+              <li>
+                <Link to="/about"> About</Link>
+              </li>
+              <li>
+                <Link to="/blogs"> Blogs</Link>
+              </li>
+            </ul>
+          </div>
+        
     {/* logo */}
       <Link to="/"><img src={logo} alt="LOGO" className="logo" /></Link>  
       
